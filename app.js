f3ef9c8177b62c1791f01469af4768a3364e625c
AdityaVikramSingh45 <adityavikramshubh234@gmail.com>
2025-05-13 17:05:48 +0530

    Undo

--- app.js
+++ app.js
@@ -93,8 +93,9 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-app.get("/", (req, res) => {
-    res.send("Welcome to Wanderlust! 🎉 Server is up and running.");
+// ✅ 404 handler
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found!"));
 });
 
 // ✅ Error handler
